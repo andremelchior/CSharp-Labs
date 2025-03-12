424b35aaee17ddfaed77b7db88d997536da95f56
André Melchior <andre.melchior@outlook.com>
2025-03-12 00:01:38 -0300

    initial commit: add simple average algorithm

---
+++ media.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, media;
+
+            Console.WriteLine("Digite a primeira nota: ");
+            n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira nota: ");
+            n3 = double.Parse(Console.ReadLine());
+            media = (n1 + n2 + n3) / 3;
+
+            Console.WriteLine("\nA média entre as notas {0}, {1} e {2} é: {3}. ", n1, n2, n3, media);
+
+            Console.ReadKey();
+
+        }
+    }
+}
