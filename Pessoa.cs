831e71af1ca402f8e5a0b4dc517d24ccc48b7920
André Melchior <andre.melchior@outlook.com>
2025-03-18 22:07:37 -0300

    initial commit: introduction to OOP

---
+++ Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introductionOOP
+{
+    internal class Pessoa
+    {
+        public double altura;
+        public double peso;
+        public string etnia;
+        public string nome;
+        public int idade;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introductionOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa aluno = new Pessoa(); //instancia é a criação de um objeto
+            Pessoa aluno1 = new Pessoa(); //instancia
+
+            aluno.nome = "tio patinhas";
+            aluno.altura = 1.70;
+            aluno.peso = 90;
+
+            aluno1.nome = "pato donald";
+
+            Console.WriteLine("Aluno: {0}", aluno1.nome);
+        }
+    }
+}
