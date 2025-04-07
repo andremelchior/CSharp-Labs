f3ce4c95ab210f719d47b6270d6e7196768a6cd0
André Melchior <andre.melchior@outlook.com>
2025-04-06 22:56:18 -0300

    feat: add class 'Cliente' and extend from 'Pessoa', implement attributes and constructor method

---
+++ exercises/Atividade_POO/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_POO
+{
+    internal class Cliente : Pessoa
+    {
+        public string cpf;
+        public string rg;
+
+        public Cliente(string cpf, string nome)
+        {
+            this.cpf = cpf;
+            this.nome = nome;
+        }
+    }
+}
