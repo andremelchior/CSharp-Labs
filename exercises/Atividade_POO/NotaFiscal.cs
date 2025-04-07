3d46d7ec6a211cbce10fa8535e3b59c2bc51db0d
André Melchior <andre.melchior@outlook.com>
2025-04-06 23:10:14 -0300

    feat: add class 'NotaFiscal' and Properties directory, add attributes and implement methods for invoice creation and product addition

---
+++ exercises/Atividade_POO/NotaFiscal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_POO
+{
+    internal class NotaFiscal
+    {
+        public double numero;
+        public Cliente Cliente;
+        public Fornecedor Fornecedor;
+        public Produto produto;
+
+        public void criaNotaFiscal(Cliente cliente, Fornecedor fornecedor)
+        {
+            Cliente = cliente;
+            Fornecedor = fornecedor;
+        }
+
+        public void adicionaProduto(Produto produto)
+        {
+            this.produto = produto;
+        }
+    }
+}
