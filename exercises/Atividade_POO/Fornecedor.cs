ac8032a1cb3c217c17172933547ee75fe2f4381b
André Melchior <andre.melchior@outlook.com>
2025-04-06 23:00:58 -0300

    feat: extend 'Fornecedor' from 'Pessoa', add attributes and implement supplier creation method

---
+++ exercises/Atividade_POO/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_POO
+{
+    internal class Fornecedor : Pessoa
+    {
+        public string cnpj;
+        public string razaosocial;
+
+        public void criaFornecedor(string nome, string cnpj)
+        {
+            this.nome = nome;
+            this.cnpj = cnpj;
+        }
+    }
+}
