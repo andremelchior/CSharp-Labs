61cc331ea2c4a8ec3550469492e7375ed9b813b0
André Melchior <andre.melchior@outlook.com>
2025-04-06 23:03:25 -0300

    feat: add class 'Produto', add attributes and implement product creation methods

---
+++ exercises/Atividade_POO/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_POO
+{
+    internal class Produto
+    {
+        public string descricao;
+        public double valor;
+
+        public double criaProduto(string descricao, double valor)
+        {
+            this.descricao = descricao;
+
+            return valor;
+        }
+
+        public void criaProduto()
+        {
+            Console.WriteLine("Digite a descrição do produto: ");
+            string descricao = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor do produto: ");
+            double valor = double.Parse(Console.ReadLine());
+               
+            this.valor = criaProduto(descricao, valor);
+        }
+    }
+}
