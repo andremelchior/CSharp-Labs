a3aa6187068ce43c163a9b450c1547c5fc170134
André Melchior <andre.melchior@outlook.com>
2025-04-06 22:36:54 -0300

    feat: add Program.cs, add attributes to class 'Pessoa'

---
+++ exercises/Atividade_POO/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_POO
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string telefone;
+        public string logradouro;
+        public string email;
+        public string usuario;
+        public string senha;
+    }
+}
