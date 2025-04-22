b4880c02f26ad93bdd4cef4f7a6f4609c2d9ef42
André Melchior <andre.silva468@fatec.sp.gov.br>
2025-04-22 19:42:04 -0300

    feat: add class "Funcionario", extends "Funcionario" from "Pessoa" and add attributes

---
+++ Funcionario.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA250325CS
+{
+    internal class Funcionario : Pessoa
+    {
+        public double salario;
+        public string cargo;
+    }
+}
