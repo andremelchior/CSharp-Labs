8d584d7deaf8a30fbaeb0ceaa1ac42f8702f31d8
André Melchior <andre.silva468@fatec.sp.gov.br>
2025-04-22 19:47:26 -0300

    feat: add "Aluno" and "Professor" instance

--- Program.cs
+++ Program.cs
@@ -13,13 +13,26 @@ namespace introductionOOP
             Pessoa aluno = new Pessoa(); //instancia é a criação de um objeto
             Pessoa aluno1 = new Pessoa(); //instancia
 
-            aluno.nome = "tio patinhas";
-            aluno.altura = 1.70;
-            aluno.peso = 90;
+            aluno.nome = "andré";
+            aluno.altura = 1.75;
+            aluno.peso = 65;
 
             aluno1.nome = "pato donald";
 
             Console.WriteLine("Aluno: {0}", aluno1.nome);
+
+            Aluno a1 = new Aluno();
+            Professor pf1 = new Professor();
+
+            pf1.nome = "Tio Patinhas";
+            a1.nome = "Huguinho";
+
+            a1.rm = 999;
+            a1.cursos = "Ciência da Computação";
+            a1.periodo = "Noturno";
+
+            pf1.salario = 1.00;
+            pf1.cargo = "Professor";
         }
     }
 }
